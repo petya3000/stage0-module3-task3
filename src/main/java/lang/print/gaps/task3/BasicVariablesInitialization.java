4fed65a1c6127417ad044045bf8f92e7e1393a2c
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-06 16:31:45 +0300

    Update BasicVariablesInitialization.java

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -3,8 +3,8 @@ package lang.print.gaps.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         int first = 1;
-        second = 10;
-        third = 100;
+        int second = 10;
+        int third = 100;
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
