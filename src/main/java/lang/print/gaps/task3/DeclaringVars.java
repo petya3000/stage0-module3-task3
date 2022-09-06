7c8db0bb2b726d9bfc8928121691f75ae9655c12
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-06 16:24:59 +0300

    Update DeclaringVars.java

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,8 +2,12 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a, b, c;
-        a = b = c = 10;
+        int a;
+        int b;
+        int c;
+        a = 10;
+        b = 10;
+        c = 10;
         System.out.println(c);
     }
 }
